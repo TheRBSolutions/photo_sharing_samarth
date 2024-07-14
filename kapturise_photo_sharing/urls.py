40fce431cc5e0d27973baf6db18cd5fdb18d282f
Rameez <Rameez-Un-Nabi>
2024-07-14 16:54:30 +0500

    second stage compeleted, basic setup

--- kapturise_photo_sharing/urls.py
+++ kapturise_photo_sharing/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from . import settings
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login'), name='home'),
     path('admin/', admin.site.urls),
      path('', include('app.urls')),
 ]
