407040c121f37b4bbab4a8ec5e37c7306b6468da
Rameez <Rameez-Un-Nabi>
2024-07-14 20:50:55 +0500

    qr_code completed, Need to build on this, also need to add in reverse urls after approval

--- qr_code/urls.py
+++ qr_code/urls.py
@@ -5,5 +5,5 @@ app_name = 'qr_code'
 
 urlpatterns = [
     path('generate/<int:album_id>/', views.generate_qr_code, name='generate'),
-    path('print/<int:album_id>/', views.print_qr_card, name='print'),
+    path('print/<int:album_id>/', views.print_qr_card, name='print_qr_card'),
 ]
